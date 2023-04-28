17f9ebfb5b2b49898cefa1b608871ad9166df292
workuanzeno <127034912+workuanzeno@users.noreply.github.com>
2023-04-28 02:54:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const customer= {firstName:"jake",
+ LastName:"smith", 
+ email:"jaekSmith!aol.com",
+ phone:"underfined",
+ zipCode:"631",
+ favoriteFlavors:"Target",
+ cupSize:"medium",
+  favoriteStore:"Target", 
+  firstVisit:"false"};
+console.log(customer)
+customer.email= "Jak3Smith1992@email.com"
+console.log(customer)
+customer.phone= 3195551234;
+console.log(customer)
+customer.zipCode=63132;
+console.log(customer)
+delete customer.zipCode;
+console.log(customer);
+delete customer.phone;
+console.log(customer)
+delete customer.favoriteStore
+console.log(customer)
+customer.futureFlavors="mango";
+console.log(customer)
+customer.toppings="chocolate";
+console.log(customer)
+customer.todaysPurchaseCost=5.32;
+console.log(customer)
+customer.phone = 3195551234;
+console.log(customer)
